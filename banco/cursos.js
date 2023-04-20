75614f7f1bb791d5de40e9b5899c82c5fc601171
ANDRESSA BEATRIZ FIDELIS LANDI COELHO <22282076@CFP127ALUNO.LOCAL>
2023-04-20 09:29:55 -0300

    a

--- banco/cursos.js
+++ banco/cursos.js
@@ -2,13 +2,13 @@ var cursos = [
     {
         "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
         "sigla" :   "DS",
-        "icone" :   "https://image.shutterstock.com/image-vector/api-interface-vector-icon-600w-659203513.jpg",
+        "icone" :   "https://cdn-icons-png.flaticon.com/512/59/59118.png",
         "carga" :   "1200",
     },
     {
         "nome"  :   "002 - Técnico em Redes de Computadores",
         "sigla" :   "RDS",
-        "icone" :   "https://img.icons8.com/ultraviolet/344/thin-client.png",
+        "icone" :   "https://cdn1.iconfinder.com/data/icons/network-sharing-12/64/network-connection-computer-technology-cyber-512.png",
         "carga" :   "1200"
     }
 ];
